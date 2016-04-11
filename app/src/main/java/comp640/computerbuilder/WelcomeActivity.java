0bbfb1bd473e6e3b1622f7f575b816bbf42c72c5
Alexander Turner <alexander.turner@aflglobal.com>
2016-04-11 10:50:53 -0400

    Welcome
    added animation to welcome screen

--- app/src/main/java/comp640/computerbuilder/WelcomeActivity.java
+++ app/src/main/java/comp640/computerbuilder/WelcomeActivity.java
@@ -1,8 +1,14 @@
 package comp640.computerbuilder;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,12 +22,17 @@ public class WelcomeActivity extends AppCompatActivity {
     /**
      * Time that the welcome animation will be shown
      */
-    private static final int WELCOME_TIME = 2000;
+    private static final int WELCOME_TIME = 2500;
 
     /**
      * The timer used for control.
      */
     private Timer _welcomeTimer;
+
+    /**
+     * The welcome image.
+     */
+    private ImageView _welcomeImage;
 
     /**
      * Sets the content view to the welcome animation
@@ -31,8 +42,20 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        _welcomeImage = (ImageView)findViewById(R.id.welcomeImageView);
         _welcomeTimer = new Timer();
 
+        animate();
+    }
+
+    /**
+     * Animates the picture.
+     */
+    private void animate(){
+        YoYo.with(Techniques.FadeIn)
+                .duration(WELCOME_TIME)
+                .playOn(_welcomeImage);
         startTimer();
     }
 
